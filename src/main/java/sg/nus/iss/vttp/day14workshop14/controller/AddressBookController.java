0e4f6234a9a48f8237e25b2c8682a0c9ca0a6414
Yenleng <yenlengggggg@gmail.com>
2023-05-19 09:43:21 +0800

    add changes

--- src/main/java/sg/nus/iss/vttp/day14workshop14/controller/AddressBookController.java
+++ src/main/java/sg/nus/iss/vttp/day14workshop14/controller/AddressBookController.java
@@ -4,12 +4,15 @@ import jakarta.validation.Valid;
 import sg.nus.iss.vttp.day14workshop14.model.Contact;
 import sg.nus.iss.vttp.day14workshop14.repository.ContactsRedis;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -38,25 +41,27 @@ public String saveAddressBook(@Valid Contact contact, BindingResult bindingResul
         model.addAttribute("successMessage", "Contact saved successfully, with status code: " +HttpStatus.CREATED +".");
     return "showContact";
 }
-/* 
-   @GetMapping("/contact/{contactId}")
-    public String getContactById(Model model, @PathVariable String contactId) {
-       Contact contact =  new Contact();
-       contact = service.getContactById(contactId, dataDir);
-        if (contact == null) {
-            model.addAttribute("errorMessage", "Contact not found");
-            return "error";
-        }
-        model.addAttribute("contact", contact);
-        return "showContact";
-    }
 
-    */
-/* 
-    @GetMapping(path = "/list")
-    public String getAllContacts(Model model) {
-        service.getAllContactInURI(model, dataDir);
-        return "contacts";
+
+@GetMapping("/contact/{contactId}")
+public String getContactById(Model model, @PathVariable String contactId) {
+   Contact contact =  new Contact();
+   contact = repository.getContactById(contactId);
+    if (contact == null) {
+        model.addAttribute("errorMessage", "Contact not found");
+        return "error";
     }
-   */ 
+    model.addAttribute("contact", contact);
+    return "showContact";
+}
+
+
+
+@GetMapping(path = "/list")
+public String getAllContacts(Model model) {
+   List<Contact> contacts = repository.getAllContacts(model);
+   model.addAttribute("contacts", contacts);
+   return "contacts";
+}
+
 }
